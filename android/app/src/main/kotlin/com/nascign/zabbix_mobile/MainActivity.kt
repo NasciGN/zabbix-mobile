083949caae21bb3d510cbafdb9e9186ae98889a2
Gabriel Nascimento <gnascimento.job@gmail.com>
2023-12-06 21:58:02 -0400

    Primeiro Commit

---
+++ android/app/src/main/kotlin/com/nascign/zabbix_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nascign.zabbix_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:zabbix_mobile/views/pages/login_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginPage(),
+  ));
+}
